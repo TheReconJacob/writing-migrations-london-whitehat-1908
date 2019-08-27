aacba52e1b82031699be3cafdeb42edae01e99a3
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-27 14:42:29 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,9 +1,2 @@
-ActiveRecord::Schema.define(version: 3) do
-
-  create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "grade"
-    t.datetime "birthdate"
-  end
-
+class CreateStudents < ActiveRecord::Migration[5.1]
 end
